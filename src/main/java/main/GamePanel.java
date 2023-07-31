ddcece53239ecf9997f3d5204f56f61e9e9278a4
MohammadMahdi <noeimohammadmahdi@gmail.com>
2023-07-31 13:36:42 +0330

    creating game thread

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 //    screen setting
     final int originalTileSize = 16;
     final int scale = 3;
@@ -13,9 +13,20 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+
     }
 }
